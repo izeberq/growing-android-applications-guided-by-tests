caddd3abe83411c1beb7c041e03dc46723ac5c01
Liudas Survila <Liudas.Survila@monitise.com>
2015-03-02 17:53:18 +0000

    - removed dependency on mock objects for smoke test, added real ones (currently) directly in test class.

--- github-android-smoke/src/test/java/com/github/frankiesardo/gaagbt/boundary/retrofit/GithubApiTest.java
+++ github-android-smoke/src/test/java/com/github/frankiesardo/gaagbt/boundary/retrofit/GithubApiTest.java
@@ -2,19 +2,27 @@ package com.github.frankiesardo.gaagbt.boundary.retrofit;
 
 import com.github.frankiesardo.gaagbt.boundary.GithubApi;
 import com.github.frankiesardo.gaagbt.entity.Repositories;
+import com.github.frankiesardo.gaagbt.entity.Repository;
 import com.github.frankiesardo.gaagbt.framework.injection.ApiLevel;
 import dagger.Module;
 import dagger.ObjectGraph;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import javax.inject.Inject;
 
-import static com.github.frankiesardo.gaagbt.boundary.mock.MockSearchRepositories.ANDROID_KEYWORD;
-import static com.github.frankiesardo.gaagbt.boundary.mock.MockSearchRepositories.ANDROID_REPOSITORIES;
 import static org.fest.assertions.Assertions.assertThat;
 
 public class GithubApiTest {
+
+    public static final String ANDROID_KEYWORD = "android";
+
+    private static final Repository GITHUB = new Repository("GitHub Android App");
+    private static final Repository COURSERA = new Repository("Source Code for Android Course Example Applications");
+    private static final Repository ACTIVE_ANDROID = new Repository("Active record style SQLite persistence for Android");
+    private static final Repositories ANDROID_REPOSITORIES = new Repositories(Arrays.asList(GITHUB, COURSERA, ACTIVE_ANDROID));
 
     @Inject
     GithubApi githubApi;
